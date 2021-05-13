26940ac2a36970f0f8eb8180e08eab7dac4b7cfb
netesh paudel <netesh.paudel@gmail.com>
2021-05-13 20:45:27 +0545

    Third commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+import 'package:carousel_slider/carousel_slider.dart';
 
 // ignore: must_be_immutable
 class Homepage extends StatelessWidget {
@@ -20,30 +23,87 @@ class Homepage extends StatelessWidget {
         ],
         elevation: 0.0,
       ),
+      drawer: Drawer(),
       body: Container(
         padding: EdgeInsets.only(top: 10, left: 30),
         child: Column(
           children: [
-            Text(
+            /*  Text(
               "Hi $name!",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.black,
-                fontStyle: FontStyle.italic,
+              style: GoogleFonts.notoSans(
+                textStyle: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-            Text(
-              "Good Evening ",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                fontStyle: FontStyle.italic,
+            Text("Good Evening ",
+                style: GoogleFonts.notoSans(
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )), */
+            CarouselSlider(
+              items: [
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/1.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/2.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/3.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                height: 180.0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                viewportFraction: 0.8,
               ),
-            )
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Column(children: [
+              Row(children: [
+                Container(
+                  child: Icon(
+                    Icons.developer_board,
+                    size: 70,
+                  ),
+                ),
+              ])
+            ])
           ],
         ),
       ),
-      drawer: Drawer(),
     );
   }
 }
