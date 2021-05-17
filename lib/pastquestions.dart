638baba6f444d06ad68213f19482c5c326e57156
P2MAVERICK <p2pokharel100@gmail.com>
2021-05-17 15:54:17 +0545

    P2Maverick

--- lib/pastquestions.dart
+++ lib/pastquestions.dart
@@ -3,58 +3,6 @@ import 'package:flutter/material.dart';
 class Pastquestions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Past Questions',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Sem('First Semester'),
-              SizedBox(height: 8.0),
-              Sem('Second Semester'),
-              SizedBox(height: 8.0),
-              Sem('Third Semester'),
-              SizedBox(height: 8.0),
-              Sem('Fourth Semester'),
-              SizedBox(height: 8.0),
-              Sem('Fifth Semester'),
-              SizedBox(height: 8.0),
-              Sem('Sixth Semester'),
-              SizedBox(height: 8.0),
-              Sem('Seventh Semester'),
-              SizedBox(height: 8.0),
-              Sem('Eighth Semester'),
-              SizedBox(height: 8.0),
-              Sem('Ninth Semester'),
-              SizedBox(height: 8.0)
-            ],
-          ),
-        ));
-  }
-}
-
-class Sem extends StatelessWidget {
-  final String name;
-
-  Sem(this.name);
-
-  @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.amberAccent),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(name),
-      ),
-    );
+    return Container();
   }
 }
