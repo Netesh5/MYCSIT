caece9cce4c4643b83f0e8890da3ec1b93c5ecbd
netesh paudel <netesh.paudel@gmail.com>
2021-05-19 23:35:09 +0545

    netesh

---
+++ lib/teachersdetail.dart
@@ -0,0 +1,32 @@
+var teachersdetail = [
+  {
+    "name": "Madhav Bhatta",
+    "address": "bhalwari",
+    "phoneno": "tel:9867838664",
+    "desp": " adfadfadf"
+  },
+  {
+    "name": "Surendra Pathak",
+    "address": "bhalwari",
+    "phoneno": "tel:9867838664",
+    "desp": " adfadfadf"
+  },
+  {
+    "name": "Mohan Bhandari",
+    "address": "bhalwari",
+    "phoneno": "tel:9867838664",
+    "desp": " adfadfadf"
+  },
+  {
+    "name": "Sudip Raj Khadka",
+    "address": "bhalwari",
+    "phoneno": "tel:9867838664",
+    "desp": " adfadfadf"
+  },
+  {
+    "name": "Ramesh singh Saud",
+    "address": "bhalwari",
+    "phoneno": "tel:9867838664",
+    "desp": " adfadfadf"
+  },
+];
