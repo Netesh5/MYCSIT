af79f0fa0574957bbd146d55532a7ba0db0e6360
netesh paudel <netesh.paudel@gmail.com>
2021-06-21 23:20:54 +0545

    netesh

--- lib/draweritems/contactus.dart
+++ lib/draweritems/contactus.dart
@@ -1,12 +1,31 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class Contactus extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Contact us"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Contact us"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 500,
+                width: MediaQuery.of(context).size.width * 0.6,
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      child: Image.asset("assets/images/elon.jpg"),
+                      borderRadius: BorderRadius.circular(30),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
