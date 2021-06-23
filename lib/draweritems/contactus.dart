85b81f79bf55f7cb225d7c368df83222bb4e8d68
netesh paudel <netesh.paudel@gmail.com>
2021-06-23 23:27:48 +0545

    contact us design

--- lib/draweritems/contactus.dart
+++ lib/draweritems/contactus.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Contactus extends StatelessWidget {
   @override
@@ -11,114 +12,147 @@ class Contactus extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                height: 600,
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      child: Image.asset("assets/images/elon.jpg"),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Prasanna Pokharel",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: Card(
+                  elevation: 7,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Container(
+                    height: 380,
+                    child: Column(
                       children: [
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.facebook,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        ClipRRect(
+                          child: Image.asset("assets/images/elon.jpg"),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        SizedBox(width: 20),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.instagram,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        SizedBox(
+                          height: 20,
                         ),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.twitter,
-                            size: 30,
+                        Text(
+                          "Prasanna Pokharel",
+                          style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                          onTap: () {},
                         ),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.github,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.facebook,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.instagram,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.twitter,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.github,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-              Container(
-                height: 600,
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Column(
-                  children: [
-                    ClipRRect(
-                      child: Image.asset("assets/images/elon.jpg"),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Nitesh Paudel",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Card(
+                  elevation: 7,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Container(
+                    height: 380,
+                    child: Column(
                       children: [
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.facebook,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        ClipRRect(
+                          child: Image.asset("assets/images/elon.jpg"),
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        SizedBox(width: 20),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.instagram,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        SizedBox(
+                          height: 20,
                         ),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.twitter,
-                            size: 30,
+                        Text(
+                          "Nitesh Paudel",
+                          style: GoogleFonts.nunito(
+                            textStyle: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
                           ),
-                          onTap: () {},
                         ),
-                        InkWell(
-                          child: FaIcon(
-                            FontAwesomeIcons.github,
-                            size: 30,
-                          ),
-                          onTap: () {},
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.facebook,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.instagram,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.twitter,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                            SizedBox(width: 20),
+                            InkWell(
+                              child: FaIcon(
+                                FontAwesomeIcons.github,
+                                size: 30,
+                              ),
+                              onTap: () {},
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
